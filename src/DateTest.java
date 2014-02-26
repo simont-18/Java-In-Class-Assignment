91daf2f4c3ac02a437d86fda9b596526bc436213
Simon T <duy.ta1895@gmail.com>
2014-02-26 12:24:03 -0600

    add while loop to keep reading in date

--- src/DateTest.java
+++ src/DateTest.java
@@ -13,21 +13,22 @@ public class DateTest {
 			in = new Scanner(new FileInputStream("untested.txt"));
 			out = new PrintWriter(new FileOutputStream("tested.txt"));
 			
-			//input format: MM/DD/YYYY
-			int m, d, y;
-			String info = in.nextLine();
-			m = Integer.parseInt(info.substring(0,2));
-			d = Integer.parseInt(info.substring(3,5));
-			y = Integer.parseInt(info.substring(6,10));
-			
-			Date InDate = new Date(m,d,y);
+			while(in.hasNext()) {
+				//input format: MM DD YYYY
+				int m, d, y;
+				m = in.nextInt();
+				d = in.nextInt();
+				y = in.nextInt();
+				
+				Date inDate = new Date(m,d,y);
 
-			for (int i = 0; i < 40; i++) {
-				InDate.nextDay();
-				System.out.println("Incremented Date: " + InDate);
-				out.println(InDate);
+				for (int i = 0; i < 40; i++) {
+					inDate.nextDay();
+					System.out.println("Incremented Date: " + inDate);
+					out.println(inDate);
+				}
 			}
-
+			
 			in.close();
 			out.close();
 		} catch (FileNotFoundException e) {
