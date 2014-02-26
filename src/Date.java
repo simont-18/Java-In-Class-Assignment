8af1740e3ac369a45f302140678a25113e3f30cd
Simon T <duy.ta1895@gmail.com>
2014-02-26 17:19:08 -0600

    update setDay to increment date into next month | change some member type to private | default for date and month is 1

--- src/Date.java
+++ src/Date.java
@@ -1,6 +1,6 @@
 public class Date {
-	int month, day, year;
-	int limit[] = { 0, 31, 28, 31, 30, 31, 30, 31, 30, 30, 31, 30, 31 };
+	private int month, day, year;
+	private int limit[] = { 0, 31, 28, 31, 30, 31, 30, 31, 30, 30, 31, 30, 31 };
 
 	Date() {
 		setall(1, 1, 2000);
@@ -12,7 +12,7 @@ public class Date {
 		System.out.println("Date object constructor for date " + toString());
 	}
 
-	public void setall(int m, int d, int y) {
+	private void setall(int m, int d, int y) {
 		setYear(y);
 		setMonth(m);
 		setDay(d);
@@ -22,40 +22,48 @@ public class Date {
 		setDay(++day);
 	}
 
-	public void setMonth(int m) {
+	private void setMonth(int m) {
 		if (m > 0 && m <= 12)
 			month = m;
 		else if (m > 0 && m > 12) {
 			month = 1;
 			setYear(++year);
-		}
+		} else if(m == 0)
+			month = 1;
 	}
 
-	public void setDay(int d) {
+	private void setDay(int d) {
 		checkLeapYear(year);
 		if (d > 0 && d <= limit[month])
 			day = d;
 		else if (d > 0 && d > limit[month]) {
-			day = 1;
+			int inc = d - limit[month];
+			day = 0;
 			setMonth(++month);
-		}
+			for(int i = 0; i < inc; i++)
+				setDay(++day);
+		} else if(d == 0)
+			day = 1;
 	}
 
-	public void setYear(int y) {
-		if (y >= 0 && y <= 2100)
+	private void setYear(int y) {
+		if (y >= 0 && y <= 2500)
 			year = y;
 		else
 			year = 2000;
+		checkLeapYear(year);
 	}
 
-	public void checkLeapYear(int y) {
+	private void checkLeapYear(int y) {
 		if (y % 4 == 0) {
 			limit[2] = 29;
-			if (y % 100 == 0)
+			if (y % 100 == 0) {
 				limit[2] = 28;
-			if (y % 400 == 0)
-				limit[2] = 29;
-		}
+				if (y % 400 == 0)
+					limit[2] = 29;
+			}
+		} else
+			limit[2] = 28;
 	}
 
 	@Override
